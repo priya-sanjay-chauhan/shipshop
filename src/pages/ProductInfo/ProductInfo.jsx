4b32afa591f3ada11f705af14453673d6beea4f8
Priya Chauhan <Priya@123>
2024-06-25 23:52:19 +0530

    created add to cart function

--- src/pages/ProductInfo/ProductInfo.jsx
+++ src/pages/ProductInfo/ProductInfo.jsx
@@ -1,87 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { ProductContext } from '../../context/ProductContext'
+import myContext from '../../context/myContext'
 
 
 const ProductInfo = () => {
-    const productData = [
-        {
-            id: 1,
-            image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-            title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 150,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        },
-        {
-            id: 2,
-            image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-            title: 'Kaushalam kalash Copper Pot',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 120,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        },
-        {
-            id: 3,
-            image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-            title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 130,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        },
-        {
-            id: 4,
-            image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-            title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 120,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        },
-        {
-            id: 1,
-            image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg',
-            title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 150,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        },
-        {
-            id: 2,
-            image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg',
-            title: 'Kaushalam kalash Copper Pot',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 120,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        },
-        {
-            id: 3,
-            image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg',
-            title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 130,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        },
-        {
-            id: 4,
-            image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg',
-            title: 'Hand Painted Blue Kaushalam Tea Pot in Aluminium',
-            desc: 'Shop Hand Painted Blue Kaushalam Tea Pot in Aluminium, handmade by Mrinalika Jain. Fair pricing. Ethically made. Positive impact.',
-            price: 120,
-            trendingProductName: 'Featured',
-            quantity: 1,
-        }
-    ]
+   const {products}=useContext(ProductContext)
+
+   const {addToCart}=useContext(myContext)
+
+   const {id}=useParams()
+   const product= products.find((item)=> item.id=== parseInt(id))
+   const handleAddToCart = () => {
+    alert("Added")
+    addToCart(product)
+ };
 
     // the useParams hook is used to extract the id parameter from the URL. This id is then used to find and display the details of the specific product.
     
-    const {id}=useParams()
-    const product= productData.find((item)=> item.id=== parseInt(id))
     return (
         <>
             <section className="py-5 lg:py-16 font-poppins ">
@@ -178,7 +114,7 @@ const ProductInfo = () => {
                                 <div className="mb-6 " />
                                 <div className="flex flex-wrap items-center mb-6">
                                    
-                                    <button
+                                    <button onClick={handleAddToCart}
                                         className="w-full px-4 py-3 text-center text-emerald-600 bg-emerald-100 border border-emerald-600  hover:bg-emerald-600 hover:text-gray-100 rounded-xl"
                                     >
                                         Add to cart
